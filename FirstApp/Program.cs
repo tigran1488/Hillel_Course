5d69377339848c7d68c0cabe90c8b55a83ae4898
tigran1488 <tigran.shvets@gmail.com>
2024-04-07 01:36:23 -0700

    add  new project

---
+++ FirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Привіт, я навчаюся у школі Hillel!");
+            Console.ReadKey();
+        }
+    }
+}
