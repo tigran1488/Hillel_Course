04fd62b71712081f6edb1402f3897c83e00cd90a
tigran1488 <tigran.shvets@gmail.com>
2024-04-07 01:41:55 -0700

    add new project

---
+++ Lesson_2/Discount/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace DiscountValue
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Task_1*/
+            double product = 10000, discount = 20;
+            double total = ((discount / 100) * product);
+            double amount = product - total;
+            Console.WriteLine("Task_1");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine($"Вартість товару до знижки={product} грн\n Відсоток знижки={discount}% \n Сума знижки={total}грн \n Кінцева вартість={amount}грн ");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
