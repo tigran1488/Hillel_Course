17ff231f039476e9c8c060221f106bd91c298de1
tigran1488 <tigran.shvets@gmail.com>
2024-04-07 01:53:54 -0700

    add new project

---
+++ Lesson_2/Book/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DiscountValue
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding=System.Text.Encoding.UTF8; 
+            string book = "Кобзарь", author = "Тарас Шевченко";
+            int price = 300, year = 1840;
+            Console.WriteLine("Task_3");
+            Console.WriteLine($"Параметри книги - назва:{book}, автор:{author}, рік:{year}, ціна:{price}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Lesson_2/Time/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Time {
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int h = 2, m = 13, s = 43;
+            int time = (s + (13 * 60) + (2 * 60 * 60));
+            Console.WriteLine("Task_2");
+            Console.WriteLine($"Загальний час (2год,13хв,43сек) у секундах={time}c");
+            Console.ReadKey();
+        }
+    }
+
+}
