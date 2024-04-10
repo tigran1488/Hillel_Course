fd633ccfb991a99c222b7ef90d8038eafed9f009
tigran1488 <tigran.shvets@gmail.com>
2024-04-10 00:52:41 -0700

    add new project

---
+++ Lesson_3/FirstTask/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FirstTask
+{
+    internal class Program
+    {
+        //1) Дано тризначне число. Знайти число, отримане під час прочитання його цифр справа наліво.
+        static void Main(string[] args)
+        {
+            int number1 = 456;
+            int hundreads,tens,one;
+            hundreads = number1 / 100;
+            tens = ((number1/10)%10);
+            one = number1 % 10;     
+            Console.WriteLine($"число:{one+tens+hundreads}");
+            Console.ReadKey();  
+        }
+    }
+}
