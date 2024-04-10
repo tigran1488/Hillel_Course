e0ace35d6410d98daedcd25f44125d36ee08d1fb
tigran1488 <tigran.shvets@gmail.com>
2024-04-10 01:16:24 -0700

    add new project

---
+++ Lesson_3/SecondTask/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SecondTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number: ");
+            double num=double.Parse(Console.ReadLine());
+            double pow = Math.Pow(num,2);
+            Console.WriteLine($"Your number squared={pow}");
+            Console.ReadKey();
+
+        }
+    }
+}
