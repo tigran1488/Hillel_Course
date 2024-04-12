4391ff3709c3fbfd76e7625eba5d086dd9e962bc
tigran1488 <tigran.shvets@gmail.com>
2024-04-11 17:32:26 -0700

    add new project

---
+++ Lesson_4/CompareNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CompareNumbers
+{
+    internal class Program
+    {
+     static void Main(string[] args)
+        {
+            Console.Write("Input first number:");
+            int a=int.Parse(Console.ReadLine());
+            Console.Write("Input second number:");
+            int b = int.Parse(Console.ReadLine());
+            if (a > b) { Console.WriteLine($"{a}>{b}"); }
+            else if (a < b) { Console.WriteLine($"{a}<{b}"); }
+            else if (a == b) { Console.WriteLine($"{a}={b}"); }
+            Console.ReadKey();
+    
+        }
+    }
+} 
