6494e2ba24902969a4afdeaf3b5fada71ae8425f
tigran1488 <tigran.shvets@gmail.com>
2024-05-03 02:07:46 -0700

    add new project

---
+++ Lesson_8/Cart/ProductType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart
+{
+    enum ProductType
+    {
+        Food =1,
+        Clothing=2,
+        Electronics=3, 
+        Books=4,
+    }
+}
---
+++ Lesson_8/Cart/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Cart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            double totalCost=0.0;
+            ProductType  productType;
+            do
+            {
+                Console.WriteLine("Введіть тип товару (1: Їжа, 2: Одяг, 3: Електроніка, 4: Книги):");
+                if (Enum.TryParse(Console.ReadLine(), out productType))
+                {
+                    Console.WriteLine("Введіть ціну товару:");
+                    if (double.TryParse(Console.ReadLine(), out double price) && price >= 0)
+                    {
+                        totalCost += price;
+                        Console.WriteLine($"Загальна вартість у кошику: {totalCost:C}");
+                    }
+                    else if (Enum.TryParse(Console.ReadLine(), out productType))
+                    {
+                       Console.WriteLine("Некоректний тип товару! Введіть 1, 2, 3 або 4");
+                    }
+                   
+                    
+                    else 
+                    {  
+                        Console.WriteLine("Некоректна ціна! Введіть додатнє число");
+                    }
+                }
+               
+                Console.WriteLine("Продовжити введення? (Y/N)");
+            } while (Console.ReadLine().Trim().ToUpper() == "Y");
+            Console.WriteLine($"Загальна вартість усіх товарів у кошику: {totalCost:C}");
+        }
+
+    } 
+}
