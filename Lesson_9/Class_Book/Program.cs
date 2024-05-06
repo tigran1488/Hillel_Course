c76b4b0fa21fc36e1b66f4db0b9a95d04cee8086
tigran1488 <tigran.shvets@gmail.com>
2024-05-06 00:53:22 -0700

    add new project

---
+++ Lesson_9/Class_Book/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Class_Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Book book = new Book();
+            book.DisplayInfo();
+            book.IsThick();
+        }
+    }
+}
---
+++ Lesson_9/Class_Book/book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Book
+{
+    public class Book
+    {
+        public string Name => "Анна Каренина";
+        public string Author => "Лев Николаевич Толстой";
+        public int Year => 1978;
+        public int Pages => 864;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Название книги - {Name},\n Автором является - {Author},\n  Первый выпуск книги был в {Year} году,\n   Книга имеет {Pages} страниц");
+        }
+        public void IsThick()
+        {
+            if (Pages > 500)
+            {
+                Console.WriteLine("Эта книга толстая!");
+            }
+        }
+
+
+    }
+}
