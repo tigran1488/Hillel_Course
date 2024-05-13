e741520768d846601a9bf60e0bd96bb3ab665439
tigran1488 <tigran.shvets@gmail.com>
2024-05-13 00:13:18 -0700

    add new  project

---
+++ Lesson_10/Person_Info/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Info
+{ 
+    
+    internal class Person
+    {
+
+        private string name;
+        private int age;
+        private string gender;
+       
+        public Person(string name,int age,string gender)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age; 
+        }
+        public Person(string name) : this(name, 0, "Не вказано")                       
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+        }
+        public Person(string name, int age) : this(name, age, "Не вказано")
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+        }
+        public void PrintDetails()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine($"Ім'я: {name}, Вік: {age}, Стать: {gender}");        
+        }
+        public bool IsAdult()
+        {
+           return age >= 18;
+        }
+        public void ChangeName(string newName) 
+        {
+            this.name = newName;    
+        }
+    }
+    
+}
---
+++ Lesson_10/Person_Info/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Person_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Person person1 = new Person("Іван", 29, "чоловік");     
+            Person person2 = new Person("Марія");
+            Person person3 = new Person("Дмитро", 42);
+
+            Console.WriteLine("Дані про першу особу:");
+            person1.PrintDetails();
+
+            Console.WriteLine("Дані про другу особу:");
+            person2.PrintDetails();
+
+            Console.WriteLine("Дані про третю особу:");
+            person3.PrintDetails();
+
+            Console.WriteLine($"Чи є перша особа дорослою? {person1.IsAdult()}");
+            Console.WriteLine($"Чи є друга особа дорослою? {person2.IsAdult()}");
+            Console.WriteLine($"Чи є третя особа дорослою? {person3.IsAdult()}");
+           
+            person1.ChangeName("Петро");
+
+            Console.WriteLine("\nПісля зміни імені першої особи:");
+            person1.PrintDetails();
+        }
+    }
+    
+}
