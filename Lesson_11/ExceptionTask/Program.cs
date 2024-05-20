18eb89e9182854661ca8cfb8268c86daa0249b40
tigran1488 <tigran.shvets@gmail.com>
2024-05-19 22:01:33 -0700

    add new project

---
+++ Lesson_11/ExceptionTask/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ExceptionTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool inputValid = false;
+            while (!inputValid)
+            {
+                try
+                {
+                    Console.Write("input your age: ");
+                    int age = int.Parse(Console.ReadLine());
+                    if (age < 0 || 100 < age)
+                    {
+                        throw new FormatException("age is not correct");
+                    }
+                    else Console.WriteLine($"your age is - {age}");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    
+                }
+              
+            }
+        }
+    }
+}
