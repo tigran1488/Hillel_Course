03b12b462c709256b30f68e625c7e38cb18169fd
tigran1488 <tigran.shvets@gmail.com>
2024-05-19 21:34:50 -0700

    add new project

---
+++ Lesson_11/ConsoleForLib/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using LibraryTask;
+namespace ConsoleForLib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            string sampleText = "У своїй сумі прислів’я та приказки становлять начебто звід правил, якими людина має керуватися у повсякденному житті.";
+
+            int vowelsCount = UkrainianStringUtils.CountVowels(sampleText);
+            int consonantsCount = UkrainianStringUtils.CountConsonants(sampleText);
+            string reversedText = UkrainianStringUtils.ReverseString(sampleText);
+            string textWithoutDuplicates = UkrainianStringUtils.RemoveDuplicates(sampleText);
+            string textWithoutPunctuation = UkrainianStringUtils.RemovePunctuation(sampleText);
+
+            Console.WriteLine("Оригінальний текст: " + sampleText);
+            Console.WriteLine("Кількість голосних: " + vowelsCount);
+            Console.WriteLine("Кількість приголосних: " + consonantsCount);
+            Console.WriteLine("Реверсований текст: " + reversedText);
+            Console.WriteLine("Текст без дублікатів: " + textWithoutDuplicates);
+            Console.WriteLine("Текст без пунктуації: " + textWithoutPunctuation);
+        }
+    }
+}
---
+++ Lesson_11/LibraryTask/UkrLib.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LibraryTask
+{
+   
+        public static class UkrainianStringUtils
+        {
+            public static int CountVowels(string input)
+            {
+                char[] vowels = { 'а', 'е', 'є', 'и', 'і', 'ї', 'о', 'у', 'ю', 'я' };
+                input = input.ToLower();
+                int count = 0;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (vowels.Contains(input[i]))
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+            public static int CountConsonants(string input)
+            {
+                char[] consonants = { 'б', 'в', 'г', 'ґ', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ' };
+                input = input.ToLower();
+                int count = 0;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (consonants.Contains(input[i]))
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+            public static string ReverseString(string input)
+            {
+                return new string(input.Reverse().ToArray());
+            }
+            public static string RemoveDuplicates(string input)
+            {
+                string result = string.Empty;
+                foreach (char c in input)
+                {
+                    if (result.IndexOf(c) == -1)
+                    {
+                        result += c;
+                    }
+                }
+                return result;
+            }
+
+            public static string RemovePunctuation(string input)
+            {
+                return Regex.Replace(input, @"\p{P}", "");
+            }
+        }
+
+    }
