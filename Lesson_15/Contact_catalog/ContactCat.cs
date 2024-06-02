977ef9981d0da84601e0d18eb5017d507c07fe25
tigran1488 <tigran.shvets@gmail.com>
2024-06-02 00:51:05 -0700

    add new project

---
+++ Lesson_15/Contact_catalog/ContactCat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_catalog
+{
+    internal class ContactCat
+    {
+        private Dictionary<string, string> directContact = new Dictionary<string, string>();
+
+        public void AddContact(string name, string phoneNumber)
+        {
+           directContact.ContainsKey(name);
+           directContact.Add(name, phoneNumber);
+           Console.WriteLine($"Контакт {name} успішно додано.");  
+        }
+        public void RemoveContact(string name)
+        {
+            directContact.Remove(name);
+            Console.WriteLine($"Контакт {name} успішно видалено.");  
+        }
+        public void SearchContact(string name)
+        {
+           directContact.ContainsKey(name);
+           Console.WriteLine($"Ім'я: {name}, Номер телефону: {directContact[name]}");  
+        }
+        public void UpdateContact(string name, string newPhoneNumber) {
+           directContact.ContainsKey(name);
+           directContact[name] = newPhoneNumber;
+           Console.WriteLine($"Контакт {name} успішно оновлено.");
+        }
+        public void ShowAllContacts()
+        {
+               Console.WriteLine("Список всіх контактів:");
+               foreach (var contact in directContact)
+               {
+                   Console.WriteLine($"Ім'я: {contact.Key}, Номер телефону: {contact.Value}");
+               }
+            
+        }
+
+    }
+}
---
+++ Lesson_15/Contact_catalog/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Contact_catalog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            ContactCat contactBook = new ContactCat();
+            string command;
+           
+            do
+            {
+                Console.WriteLine("\nВиберіть операцію:");
+                Console.WriteLine("1. Додати контакт");
+                Console.WriteLine("2. Видалити контакт");
+                Console.WriteLine("3. Оновити номер телефону");
+                Console.WriteLine("4. Шукати контакт");
+                Console.WriteLine("5. Вивести всі контакти");
+                Console.WriteLine("6. Вийти");
+                Console.Write("Введіть номер операції: ");
+                command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "1":
+                        Console.Write("Введіть ім'я: ");
+                        string nameToAdd = Console.ReadLine();
+                        Console.Write("Введіть номер телефону: ");
+                        string phoneNumberToAdd = Console.ReadLine();
+                        contactBook.AddContact(nameToAdd, phoneNumberToAdd);
+                        break;
+                    case "2":
+                        Console.Write("Введіть ім'я для видалення: ");
+                        string nameToRemove = Console.ReadLine();
+                        contactBook.RemoveContact(nameToRemove);
+                        break;
+                    case "3":
+                        Console.Write("Введіть ім'я для оновлення: ");
+                        string nameToUpdate = Console.ReadLine();
+                        Console.Write("Введіть новий номер телефону: ");
+                        string newPhoneNumber = Console.ReadLine();
+                        contactBook.UpdateContact(nameToUpdate, newPhoneNumber);
+                        break;
+                    case "4":
+                        Console.Write("Введіть ім'я для пошуку: ");
+                        string nameToSearch = Console.ReadLine();
+                        contactBook.SearchContact(nameToSearch);
+                        break;
+                    case "5":
+                        contactBook.ShowAllContacts();
+                        break;
+                    case "6":
+                        Console.WriteLine("Вихід з програми.");
+                        break;
+                   
+                }
+            } while (command != "6");
+
+        }
+    }
+}
